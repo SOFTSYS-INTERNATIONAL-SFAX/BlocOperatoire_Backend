d4ca6fcdf934dbe23bcfa30adf9a5a316769d873
Aymen Ouerghemmi <159343354+AymeenOuerghemmi@users.noreply.github.com>
2026-02-09 21:10:30 +0100

    chore: initialize spring boot backend environment

---
+++ src/main/java/com/tn/softsys/blocoperatoire/BlocOperatoireApplication.java
@@ -0,0 +1,12 @@
+package com.tn.softsys.blocoperatoire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlocOperatoireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlocOperatoireApplication.class, args);
+    }
+}
